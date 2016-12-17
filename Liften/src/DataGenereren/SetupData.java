c29f426c81b549486bcf88457e54b891948b3e4f
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-12-17 20:15:29 +0100

    probleem generatie

--- Liften/src/DataGenereren/SetupData.java
+++ Liften/src/DataGenereren/SetupData.java
@@ -1,8 +1,10 @@
 package DataGenereren;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -14,10 +16,18 @@ public class SetupData extends Application {
 		primaryStage.setTitle("Event Handler Demo");
 		
 		try {
-			FXMLLoader loader = new FXMLLoader(SetupData.class.getResource("Setup.fxml"));
-			AnchorPane page = (AnchorPane) loader.load();
-			Scene scene = new Scene(page);
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader();
+			InputStream s = null;
+			try{
+				s = getClass().getClassLoader().getResource("Setup.fxml").openStream();
+			}catch(Exception e){
+
+			}
+			Parent root = (Parent) loader.load(s);
+
+			//Setten van enkele elementen van het hoofdscherm
+			primaryStage.setTitle("Liften");
+			primaryStage.setScene(new Scene(root));
 			primaryStage.show();
 		} catch (IOException e) {
 			System.err.println("Error loading EventHandlerDemo.fxml!");
